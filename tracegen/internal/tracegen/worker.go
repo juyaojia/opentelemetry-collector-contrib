bdeeb0f0aa483fdc9d0981aa9a869e963d4a22bb
Georgia <Georgia@jyjdeMBP.lan>
2022-06-24 14:21:24 -0700

    enable worker to simulate two different trace structure at one time, add fake attributes to each span of those two trace types

--- tracegen/internal/tracegen/worker.go
+++ tracegen/internal/tracegen/worker.go
@@ -16,35 +16,37 @@
 package tracegen // import "github.com/open-telemetry/opentelemetry-collector-contrib/tracegen/internal/tracegen"
 
 import (
-	"context"
-	"sync"
-	"sync/atomic"
-	"time"
-
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
+        "context"
+        "sync"
+        "sync/atomic"
+        "time"
+        "math/rand"
+        "go.opentelemetry.io/otel"
+        "go.opentelemetry.io/otel/attribute"
     sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
-	"go.opentelemetry.io/otel/trace"
-	"go.uber.org/zap"
-	"golang.org/x/time/rate"
+        semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+        "go.opentelemetry.io/otel/trace"
+        "go.uber.org/zap"
+        "golang.org/x/time/rate"
 )
 
 type worker struct {
-	running          *uint32         // pointer to shared flag that indicates it's time to stop the test
-	numTraces        int             // how many traces the worker has to generate (only when duration==0)
-	propagateContext bool            // whether the worker needs to propagate the trace context via HTTP headers
-	totalDuration    time.Duration   // how long to run the test for (overrides `numTraces`)
-	limitPerSecond   rate.Limit      // how many spans per second to generate
-	wg               *sync.WaitGroup // notify when done
-	logger           *zap.Logger
+        running          *uint32         // pointer to shared flag that indicates it's time to stop the test
+        numTraces        int             // how many traces the worker has to generate (only when duration==0)
+        propagateContext bool            // whether the worker needs to propagate the trace context via HTTP headers
+        totalDuration    time.Duration   // how long to run the test for (overrides `numTraces`)
+        limitPerSecond   rate.Limit      // how many spans per second to generate
+        wg               *sync.WaitGroup // notify when done
+        logger           *zap.Logger
+        traceTypes        int
+        serviceNames      [12]string
     tracerProviders  []*sdktrace.TracerProvider
 }
 
 const (
-	fakeIP string = "1.2.3.4"
+        fakeIP string = "1.2.3.4"
 
-	fakeSpanDuration = 123 * time.Microsecond
+        fakeSpanDuration = 123 * time.Microsecond
 )
 
 func (w worker) setUpTracers() []trace.Tracer {
@@ -58,58 +60,155 @@ func (w worker) setUpTracers() []trace.Tracer {
     return toReturn
 }
 
-func (w worker) addChild(parentCtx context.Context, tracer trace.Tracer) context.Context {
-    childCtx, child := tracer.Start(parentCtx, "okey-dokey", trace.WithAttributes(
-        attribute.String("span.kind", "server"),
-        semconv.NetPeerIPKey.String(fakeIP),
-        semconv.PeerServiceKey.String("tracegen-client"),
+func (w worker) addChild(parentCtx context.Context, tracer trace.Tracer, message string, serviceName string, httpStatusCode string, httpUrl string) context.Context {
+    childCtx, child := tracer.Start(parentCtx, message, trace.WithAttributes(
+        attribute.String("span.kind", getRandSpanKind()), // is there a semantic convention for this?
+        attribute.String("service.name", serviceName),
+        semconv.HTTPStatusCodeKey.String(httpStatusCode),
+        semconv.HTTPURLKey.String(httpUrl),
     ))
     opt := trace.WithTimestamp(time.Now().Add(fakeSpanDuration))
     child.End(opt)
     return childCtx
 }
 
+// input a range and get a random number within that range
+func getRandomNum(min int, max int) int{
+    rand.Seed(time.Now().UnixNano())
+    return rand.Intn(max - min+1) + min
+}
+
+
+// get a status code based on hardcoded probability
+func getRandStatusCode() string {
+    var statusCode string
+    randNum := getRandomNum(1, 10)
+    switch randNum {
+        case 1, 2, 3, 4, 5: statusCode = "200"
+        case 6: statusCode = "500"
+        case 7: statusCode = "202"
+        case 8: statusCode = "402"
+        case 9: statusCode = "300"
+        case 10: statusCode = "404"
+        // just in case of error
+        default: statusCode = "Error"
+    }
+    return statusCode
+}
+
+func getRandSpanKind() string {
+    var spanKind string
+    randNum := getRandomNum(1, 2)
+    switch randNum {
+        case 1: spanKind = "client"
+        case 2: spanKind = "server"
+        // just in case of error
+        default: spanKind = "N/A"
+    }
+    return spanKind
+}
+
+// need exception handling!
+func (w worker) getRootAttribute(servicesIndex int) (string, string, string, string){
+    //one status code and url for entire tree
+    spanKind := getRandSpanKind()
+    serviceName := w.serviceNames[servicesIndex]
+    httpStatusCode := getRandStatusCode()
+    httpUrl := "http://metadata.google.internal/computeMetadata/v1/instance/attributes/cluster-name(fakeurl)"
+    return spanKind, serviceName, httpStatusCode, httpUrl
+}
+
+
 func (w worker) simulateTraces() {
     // set up all tracers
     tracers := w.setUpTracers()
-	limiter := rate.NewLimiter(w.limitPerSecond, 1)
-	var i int
-	for atomic.LoadUint32(w.running) == 1 {
-		ctx, sp := tracers[0].Start(context.Background(), "lets-go", trace.WithAttributes(
-			attribute.String("span.kind", "client"), // is there a semantic convention for this?
-			semconv.NetPeerIPKey.String(fakeIP),
-			semconv.PeerServiceKey.String("tracegen-server"),
-		))
+    limiter := rate.NewLimiter(w.limitPerSecond, 1)
+    var i int
+    for atomic.LoadUint32(w.running) == 1 {
+        t := i%w.traceTypes
+        if t == 0 {
+            w.simulateTrace1(limiter, tracers)
+        } else if t == 1 {
+            w.simulateTrace2(limiter, tracers)
+        }
+        i++
+        if w.numTraces != 0 {
+                if i >= w.numTraces {
+                        break
+                }
+        }
+    }
+    w.logger.Info("traces generated", zap.Int("traces", i))
+    w.wg.Done()
+}
 
-        //childCtx := w.addChild(ctx, tracers[1])
-        //_ = w.addChild(childCtx, tracers[2])
 
-        child1Ctx := w.addChild(ctx, tracers[1])
-        w.addChild(ctx, tracers[1])
-        w.addChild(child1Ctx, tracers[2])
-        w.addChild(child1Ctx, tracers[3])
-        w.addChild(child1Ctx, tracers[4])
-        grandchild4Ctx := w.addChild(child1Ctx, tracers[5])
-        w.addChild(grandchild4Ctx, tracers[6])
-        w.addChild(grandchild4Ctx, tracers[7])
-        w.addChild(grandchild4Ctx, tracers[8])
-        w.addChild(grandchild4Ctx, tracers[9])
-        w.addChild(grandchild4Ctx, tracers[10])
-        /*
-        w.addChild(grandchild4Ctx, tracers[11])
-        */
-		limiter.Wait(context.Background())
+func (w worker) simulateTrace1(limiter *rate.Limiter, tracers []trace.Tracer) {
+    spanKind, serviceName, httpStatusCode, httpUrl := w.getRootAttribute(0)
+    ctx, sp := tracers[0].Start(context.Background(), "lets-go", trace.WithAttributes(
+        attribute.String("span.kind", spanKind), // is there a semantic convention for this?
+        attribute.String("service.name", serviceName),
+        semconv.HTTPStatusCodeKey.String(httpStatusCode),
+        semconv.HTTPURLKey.String(httpUrl),
+    ))
 
-        opt := trace.WithTimestamp(time.Now().Add(fakeSpanDuration))
-		sp.End(opt)
+    child1Ctx := w.addChild(ctx, tracers[0], "1", w.serviceNames[0], httpStatusCode, httpUrl)
+    w.addChild(child1Ctx, tracers[4], "11", w.serviceNames[4], httpStatusCode, httpUrl)
 
-		i++
-		if w.numTraces != 0 {
-			if i >= w.numTraces {
-				break
-			}
-		}
-	}
-	w.logger.Info("traces generated", zap.Int("traces", i))
-	w.wg.Done()
+    w.addChild(ctx, tracers[0], "2", w.serviceNames[0], httpStatusCode, httpUrl)
+
+    child3Ctx := w.addChild(ctx, tracers[0], "3", w.serviceNames[3], httpStatusCode, httpUrl)
+    grandchild3Ctx := w.addChild(child3Ctx, tracers[8], "31", w.serviceNames[8], httpStatusCode, httpUrl)
+    greatgrandchild3Ctx := w.addChild(grandchild3Ctx, tracers[8], "32", w.serviceNames[8], httpStatusCode, httpUrl)
+    w.addChild(greatgrandchild3Ctx, tracers[7], "33", w.serviceNames[7], httpStatusCode, httpUrl)
+
+    for i := 0; i< 5; i++ {
+        child4Ctx := w.addChild(ctx, tracers[0], "4", w.serviceNames[0], httpStatusCode, httpUrl)
+        w.addChild(child4Ctx, tracers[7], "41", w.serviceNames[7], httpStatusCode, httpUrl)
+    }
+
+    child5Ctx := w.addChild(ctx, tracers[0], "5", w.serviceNames[0], httpStatusCode, httpUrl)
+    w.addChild(child5Ctx, tracers[10], "51", w.serviceNames[10], httpStatusCode, httpUrl)
+
+    for i := 0; i< 2; i++ {
+        child6Ctx := w.addChild(ctx, tracers[0], "6", w.serviceNames[0], httpStatusCode, httpUrl)
+        w.addChild(child6Ctx, tracers[4], "61", w.serviceNames[4], httpStatusCode, httpUrl)
+    }
+
+    limiter.Wait(context.Background())
+    opt := trace.WithTimestamp(time.Now().Add(fakeSpanDuration))
+    sp.End(opt)
+}
+
+func (w worker) simulateTrace2(limiter *rate.Limiter, tracers []trace.Tracer) {
+   spanKind, serviceName, httpStatusCode, httpUrl := w.getRootAttribute(0)
+   ctx, sp := tracers[0].Start(context.Background(), "lets-go", trace.WithAttributes(
+        attribute.String("span.kind", spanKind), // is there a semantic convention for this?
+        attribute.String("service.name", serviceName),
+        semconv.HTTPStatusCodeKey.String(httpStatusCode),
+        semconv.HTTPURLKey.String(httpUrl),
+    ))
+
+
+    child1Ctx := w.addChild(ctx, tracers[0], "1", w.serviceNames[0],httpStatusCode, httpUrl)
+    w.addChild(child1Ctx, tracers[4], "11", w.serviceNames[4], httpStatusCode, httpUrl)
+
+    child2Ctx := w.addChild(ctx, tracers[0], "2", w.serviceNames[0], httpStatusCode, httpUrl)
+    w.addChild(child2Ctx, tracers[7], "21", w.serviceNames[7], httpStatusCode, httpUrl)
+
+    w.addChild(ctx, tracers[0], "3", w.serviceNames[0], httpStatusCode, httpUrl)
+
+    for i := 0; i< 9; i++ {
+        child4Ctx := w.addChild(ctx, tracers[0], "4", w.serviceNames[0], httpStatusCode, httpUrl)
+        w.addChild(child4Ctx, tracers[4], "41", w.serviceNames[4], httpStatusCode, httpUrl)
+    }
+
+    child5Ctx := w.addChild(ctx, tracers[0], "5", w.serviceNames[0], httpStatusCode, httpUrl)
+    w.addChild(child5Ctx, tracers[1], "51", w.serviceNames[1], httpStatusCode, httpUrl)
+
+    limiter.Wait(context.Background())
+
+    opt := trace.WithTimestamp(time.Now().Add(fakeSpanDuration))
+    sp.End(opt)
+
 }
